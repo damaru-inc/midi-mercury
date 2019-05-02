ab8569768bae0f4f04745abd4ec1dd73203509e1
Michael Davis <damaru.inc@gmail.com>
2019-05-01 23:05:14 -0400

    Made the receiver work.

--- src/main/java/com/damaru/midimercury/Main.java
+++ src/main/java/com/damaru/midimercury/Main.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 import javax.sound.midi.MidiDevice;
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.ShortMessage;
+import javax.sound.midi.Transmitter;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -17,11 +18,11 @@ public class Main {
     public static void log(String msg) {
         System.out.println(msg);
     }
-    
+
     public static void prompt(String msg) {
         System.out.print(msg + ": ");
     }
-    
+
     public static void main(String[] args) throws Exception {
 
         Options options = new Options();
@@ -32,99 +33,71 @@ public class Main {
         options.addOption("t", "to midi");
         options.addRequiredOption("u", "username", true, "username");
         options.addRequiredOption("v", "vpn", true, "vpn");
-        
+        options.addOption("x", "test");
+
         CommandLineParser parser = new DefaultParser();
-        CommandLine cmd = parser.parse( options, args);
-        
+        CommandLine cmd = parser.parse(options, args);
+
+        boolean test = cmd.hasOption('x');
+
+        if (test) {
+            testPerf(cmd);
+        } else {
+            run(cmd);
+        }
+    }
+
+    private static void run(CommandLine cmd) throws Exception {
         boolean from = cmd.hasOption('f');
         boolean to = cmd.hasOption('t');
-        
+
         if (!from && !to) {
             from = true;
             to = true;
         }
-        
+
         log("from " + from + " to " + to + " binary: " + cmd.hasOption('b'));
-        
-        SolacePublisher publisher = new SolacePublisher(cmd);
-        
-        //int numMessages = 10000;
-        int numMessages = 400000;
-        
-        String text = "Support for nested build without a settings file was deprecated and will be removed in Gradle 5.0.";
-        String topic = "a/a";
-        
-        MidiReceiver mr = new MidiReceiver(cmd, publisher);
-        
-        log("" + (new Date()) + " About to send midi via text.");
-        mr.setBinary(false);
-        ShortMessage sm = new ShortMessage(128, 2, 3, 4);
-        for (int i = 0; i < numMessages; i++) {
-            mr.send(sm, 0);
-        }
-        log("" + (new Date()) + " Finish sending midi.");
-        
-        Thread.sleep(5000);
-        
-        log("" + (new Date()) + " About to send midi via binary.");
-        mr.setBinary(true);
-        for (int i = 0; i < numMessages; i++) {
-            mr.send(sm, 0);
-        }
-        
-        log("" + (new Date()) + " Finish sending midi.");
 
-        mr.close();
-        
-        
-//        
-//        log("" + (new Date()) + " About to send text.");
-//        for (int i = 0; i < numMessages; i++) {
-//            publisher.sendText(text, topic);
-//        }
-//        log("" + (new Date()) + " Finish sending text.");
-//        byte[] data = text.getBytes();
-//        
-//        Thread.sleep(5000);
-//        log("" + (new Date()) + " About to send bytes.");
-//        for (int i = 0; i < numMessages; i++) {
-//            publisher.sendBinary(data, topic);
-//        }
-//        
-//        log("" + (new Date()) + " Finish sending text.");
-
-        if (false) {
-        
         MidiDevice fromDevice = null;
         MidiDevice toDevice = null;
-        
+
         if (from) {
             fromDevice = pickDevice("Select the midi device to read from");
+
+            Transmitter transmitter = fromDevice.getTransmitter();
+
+            if (transmitter != null) {
+                SolacePublisher publisher = new SolacePublisher(cmd);
+                MidiReceiver midiReceiver = new MidiReceiver(cmd, publisher);
+                fromDevice.open();
+                transmitter.setReceiver(midiReceiver);
+            }
         }
-        
+
         if (to) {
-            toDevice = pickDevice("Select the midi device to write to");
+            //toDevice = pickDevice("Select the midi device to write to");
         }
-        }
+
     }
-    
+
     private static MidiDevice pickDevice(String prompt) throws Exception {
         MidiDevice ret = null;
-        
+
         int i = 1;
         MidiDevice.Info infos[] = MidiSystem.getMidiDeviceInfo();
-        
+
         for (MidiDevice.Info info : infos) {
-            String desc = String.format("%2d: %s %s from %s version %s", i++, info.getName(), info.getDescription(), info.getVendor());
+            String desc = String.format("%2d: %s %s from %s version %s", i++, info.getName(), info.getDescription(),
+                    info.getVendor(), info.getVersion());
             log(desc);
         }
 
         prompt(prompt);
         Scanner scanner = new Scanner(System.in);
-        
+
         boolean ok = false;
         int num = 0;
-        
+
         while (!ok) {
             try {
                 num = scanner.nextInt();
@@ -138,12 +111,61 @@ public class Main {
                 log("Invalid selection: " + input);
             }
         }
-        
+
         scanner.close();
-        
-        MidiDevice.Info info = infos[num-1];
+
+        MidiDevice.Info info = infos[num - 1];
         ret = MidiSystem.getMidiDevice(info);
         log("You picked " + info.getName());
         return ret;
+    }
+
+    private static void testPerf(CommandLine cmd) throws Exception {
+        SolacePublisher publisher = new SolacePublisher(cmd);
+
+        // int numMessages = 10000;
+        int numMessages = 400000;
+
+
+        MidiReceiver mr = new MidiReceiver(cmd, publisher);
+
+        log("" + (new Date()) + " About to send midi via text.");
+        mr.setBinary(false);
+        ShortMessage sm = new ShortMessage(128, 2, 3, 4);
+        for (int i = 0; i < numMessages; i++) {
+            mr.send(sm, 0);
+        }
+        log("" + (new Date()) + " Finish sending midi.");
+
+        Thread.sleep(5000);
+
+        log("" + (new Date()) + " About to send midi via binary.");
+        mr.setBinary(true);
+        for (int i = 0; i < numMessages; i++) {
+            mr.send(sm, 0);
+        }
+
+        log("" + (new Date()) + " Finish sending midi.");
+
+        mr.close();
+
+//        String text = "Support for nested build without a settings file was deprecated and will be removed in Gradle 5.0.";
+//        String topic = "a/a";
+        //
+        // log("" + (new Date()) + " About to send text.");
+        // for (int i = 0; i < numMessages; i++) {
+        // publisher.sendText(text, topic);
+        // }
+        // log("" + (new Date()) + " Finish sending text.");
+        // byte[] data = text.getBytes();
+        //
+        // Thread.sleep(5000);
+        // log("" + (new Date()) + " About to send bytes.");
+        // for (int i = 0; i < numMessages; i++) {
+        // publisher.sendBinary(data, topic);
+        // }
+        //
+        // log("" + (new Date()) + " Finish sending text.");
+
     }
 }
